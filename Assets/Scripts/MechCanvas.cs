2c1108a9b66b457fe1d487def25b015680919ee2
FANTOMz3 <rostislavirbit@gmail.com>
2022-01-12 19:02:33 +0500

    Размножение Ui mech

--- Assets/Scripts/MechCanvas.cs
+++ Assets/Scripts/MechCanvas.cs
@@ -1,11 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class MechCanvas : MonoBehaviour
 {
     public void toggleUI()
     {
         gameObject.SetActive(!gameObject.activeSelf);
+    }
+    public void canvasoff()
+    {
+        if (gameObject.activeSelf == true)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+    public void buttonmove()
+    {
+        
     }
 }
